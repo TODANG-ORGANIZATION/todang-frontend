da8038919d765aa2bef0594483517d4c1bf74f37
david8943 <david8943@naver.com>
2025-02-04 17:32:36 +0900

    Design: Header, Footer, Layout.js, App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/pratice';
 import './index.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import MainPage from "./pages/MainPage";
+import CartPage from "./pages/CartPage";
+import PaymentPage from "./pages/PaymentPage";
+import OrderHistoryPage from './pages/OrderHistoryPage';
+import OrderDetailPage from './pages/OrderDetailPage';
+import ReviewWritePage from './pages/ReviewWritePage';
+import LikedPage from './pages/LikedPage';
+import LoginSelectPage from './pages/LoginSelectPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* 로그인페이지 */}
-          <Route path="/" element={<LoginPage />} />
-          </Routes>
+          <Route path="/login-select" element={<LoginSelectPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="payment" element={<PaymentPage />} />
+            <Route path="orders" element={<OrderHistoryPage />} />
+            <Route path="order/:id" element={<OrderDetailPage />} />
+            <Route path="review/write/:orderId" element={<ReviewWritePage />} />
+            <Route path="liked" element={<LikedPage />} />
+          </Route>
+        </Routes>
       </div>
     </Router>
   );
